0e4a9f0e859405a18c9b6a76f6e2dac31ff28f51
Batschko <lukas.h2@hotmail.de>
2020-12-22 19:20:53 +0100

    removed unused vars and comments

--- app/src/main/java/de/codingforcelm/idmp/fragment/ListPlayer.java
+++ app/src/main/java/de/codingforcelm/idmp/fragment/ListPlayer.java
@@ -16,11 +16,7 @@ import de.codingforcelm.idmp.R;
 import de.codingforcelm.idmp.player.service.MusicService;
 
 public class ListPlayer extends Fragment {
-    private ListView songView;
     private List<PhysicalSong> songList;
-    private MusicService service;
-    // The onCreateView method is called when Fragment should create its View object hierarchy,
-    // either dynamically or via XML layout inflation.
 
     public ListPlayer(List<PhysicalSong> songList) {
         this.songList=songList;
@@ -29,14 +25,13 @@ public class ListPlayer extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
         return inflater.inflate(R.layout.player_list, parent, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         FragmentTransaction ft = getChildFragmentManager().beginTransaction();
-        ft.replace(R.id.player_list_bot, new ControlsSmall(service), "CONTROLS_SMALL");
+        ft.replace(R.id.player_list_bot, new ControlsSmall(), "CONTROLS_SMALL");
         ft.replace(R.id.player_list_top, new SongList(songList), "SONGLIST");
         ft.commit();
     }
