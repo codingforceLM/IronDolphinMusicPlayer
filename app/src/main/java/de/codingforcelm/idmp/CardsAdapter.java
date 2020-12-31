c4a3a36af54a46651bc594c650483af06e4991d5
Batschko <lukas.h2@hotmail.de>
2020-12-31 17:30:12 +0100

    implementation context menu

--- app/src/main/java/de/codingforcelm/idmp/CardsAdapter.java
+++ app/src/main/java/de/codingforcelm/idmp/CardsAdapter.java
@@ -1,10 +1,17 @@
 package de.codingforcelm.idmp;
 
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +19,8 @@ import java.util.ArrayList;
 
 public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHolder> {
     private ArrayList<PhysicalSong> songList;
+    public static final String LOG_TAG = "CardsAdapter";
+    private onLongItemClickListener longClickListener;
 
     public static class CardViewHolder extends RecyclerView.ViewHolder {
         public ImageView item_image;
@@ -24,6 +33,7 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHold
             item_title = itemView.findViewById(R.id.item_title);
             item_artist = itemView.findViewById(R.id.item_artist);
         }
+        
     }
 
     public CardsAdapter(ArrayList<PhysicalSong> songList) {
@@ -43,6 +53,15 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHold
         holder.item_title.setText(currentItem.getTitle());
         holder.item_artist.setText(currentItem.getArtist());
         holder.itemView.setTag(position);
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (longClickListener != null) {
+                    longClickListener.ItemLongClicked(v, position);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
@@ -50,5 +69,12 @@ public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHold
         return songList.size();
     }
 
+    public void setOnLongItemClickListener(onLongItemClickListener onLongItemClickListener) {
+        this.longClickListener = onLongItemClickListener;
+    }
+
+    public interface onLongItemClickListener {
+        void ItemLongClicked(View v, int position);
+    }
 
 }
