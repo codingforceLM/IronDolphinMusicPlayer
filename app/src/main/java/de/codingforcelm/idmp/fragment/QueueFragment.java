ddabc9a2593c2cb4eed4d5f8e3c63db242eba0f5
mbl <mbl.85@web.de>
2021-01-06 21:47:52 +0100

    Added fragment for queue

--- app/src/main/java/de/codingforcelm/idmp/fragment/QueueFragment.java
+++ app/src/main/java/de/codingforcelm/idmp/fragment/QueueFragment.java
@@ -42,7 +42,7 @@ public class QueueFragment extends NameAwareFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_playlist, container, false);
+        return inflater.inflate(R.layout.fragment_queue, container, false);
     }
 
     @Override
