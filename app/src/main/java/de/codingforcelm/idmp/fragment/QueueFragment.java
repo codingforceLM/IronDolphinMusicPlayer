441db1ccdf934226dfe33c4a1bc95c67709e6346
Batschko <lukas.h2@hotmail.de>
2021-01-06 20:43:32 +0100

    added queue fragment

--- app/src/main/java/de/codingforcelm/idmp/fragment/QueueFragment.java
+++ app/src/main/java/de/codingforcelm/idmp/fragment/QueueFragment.java
@@ -9,9 +9,9 @@ import androidx.fragment.app.Fragment;
 
 import de.codingforcelm.idmp.R;
 
-public class TestFragment extends NameAwareFragment {
+public class QueueFragment extends NameAwareFragment {
 
-    public TestFragment() {
+    public QueueFragment() {
         //needed default constructor
         setFragmentname(this.getClass().getSimpleName());
     }
@@ -27,11 +27,22 @@ public class TestFragment extends NameAwareFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        return inflater.inflate(R.layout.fragment_playlist, container, false);
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        if (getChildFragmentManager().findFragmentByTag(ControlsFragment.class.getSimpleName()) != null) {
+            getChildFragmentManager().beginTransaction().attach(getChildFragmentManager().findFragmentByTag(ControlsFragment.class.getSimpleName())).commit();
+
+        } else {
+            getChildFragmentManager().beginTransaction().add(
+                    R.id.pl_controls_frame,
+                    new ControlsFragment(),
+                    ControlsFragment.class.getSimpleName()
+            ).commit();
+        }
+
 
     }
 }
